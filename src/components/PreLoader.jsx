04a003371a8bcd0f2d07bcfe2f3b3c4cd26a4ffb
Ahmad Saulawa <ahmadlawal020@gmail.com>
2025-05-08 12:45:28 +0100

    Loader update

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -1,16 +1,11 @@
-import { ThreeDots } from "react-loader-spinner";
+import { BeatLoader } from "react-spinners";
 
 const PreLoader = ({ visible = true }) => {
+  if (!visible) return null;
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-70 z-50">
-      <ThreeDots
-        visible={visible}
-        height="80"
-        width="80"
-        color="#3b82f6" // Tailwind blue-500
-        radius="9"
-        ariaLabel="three-dots-loading"
-      />
+      <BeatLoader color="#3b82f6" size={15} />
     </div>
   );
 };
